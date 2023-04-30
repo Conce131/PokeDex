0a55a142685380afe2cf267c2dece48244b7fbf7
Carlos Concepcion <concep131@gmail.com>
2023-04-30 18:16:34 +0100

    Arreglando conflicto entre tailwind y veutify

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,27 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-card @click="ShowDialog = true">
+    <v-card-title> Título de la tarjeta </v-card-title>
+    <v-card-text> Contenido de la tarjeta </v-card-text>
+    <v-dialog v-model="ShowDialog">
+      <v-card>
+        <v-card-title> Título del diálogo </v-card-title>
+        <v-card-text> Contenido del diálogo </v-card-text>
+      </v-card>
+    </v-dialog>
+  </v-card>
 </template>
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const ShowDialog = ref(false)
+    return {
+      ShowDialog
+    }
+  }
+}
+</script>
 
 <style>
 @media (min-width: 1024px) {
--- src/views/AnotherView.vue
+++ src/views/AnotherView.vue
@@ -1,28 +1,65 @@
 <template>
   <div>
-    <p>this is my anotherview</p>
-    <ul>
-      <li v-for="(item, index) in pokeList" :key="index">
-        <RouterLink :to="'/pokemon/' + (index + 1)">
-          {{ item.name }}
+    <v-card
+      @click="showDialog(item)"
+      v-for="(item, index) in pokeList"
+      :key="index"
+      :class="{ 'bg-orange': true, 'm-2': true }"
+    >
+      <v-card-title> {{ item.name }} </v-card-title>
+      <img :src="imageSrc + (index + 1) + '.png'" alt="Pokemon" />
+      <v-card-text> Contenido de la tarjeta </v-card-text>
+    </v-card>
+    <v-dialog v-model="dialogVisible">
+      <v-card>
+        <RouterLink :to="'/pokemon/' + selectedPokemon.id">
+          <v-card-title>{{ selectedPokemon.name }}</v-card-title>
         </RouterLink>
-        <RouterView />
-      </li>
-    </ul>
+        <img :src="selectedPokemon.image" />
+
+        <v-card-text>{{ selectedPokemon.description }}</v-card-text>
+        <v-card-actions>
+          <v-btn @click="dialogVisible = false">Cerrar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
 <script type="module">
 import { ref, onMounted } from 'vue'
 import getResponse from '../modules/api.js'
+import axios from 'axios'
 export default {
   setup() {
     const pokeList = ref([])
+    const imageSrc =
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/'
+    const selectedPokemon = ref(null)
+    const dialogVisible = ref(false)
+
     onMounted(async () => {
-      const response = await getResponse(`https://pokeapi.co/api/v2/pokemon?limit=151`) // Usar $api.getResponse()
-      pokeList.value = response.results
+      const response = await getResponse(`https://pokeapi.co/api/v2/pokemon?limit=151`)
+      response.results.forEach((pokemon, index) => {
+        pokeList.value.push({
+          id: index + 1,
+          name: pokemon.name,
+
+          url: pokemon.url
+        })
+      })
     })
-    return { pokeList }
+    async function showDialog(pokemon) {
+      const response = await axios.get(pokemon.url)
+      selectedPokemon.value = response.data
+      selectedPokemon.value.image = imageSrc + selectedPokemon.value.id + '.png'
+      dialogVisible.value = true
+    }
+    function saveSelectedPokemon() {
+      localStorage.setItem('selectedPokemon', JSON.stringify(selectedPokemon.value))
+    }
+
+    return { pokeList, imageSrc, selectedPokemon, dialogVisible, showDialog, saveSelectedPokemon }
   }
 }
 </script>
