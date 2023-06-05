bf2706b27114abb3673a4bf9606df0add43beff0
Conce131 <you@econcep131@gmail.com>
2023-06-05 17:50:25 +0100

    Trying to create evolution section

--- src/components/PokeEvolution.vue
+++ src/components/PokeEvolution.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>{{ species.evolution_chain }}</div>
+  <div>{{ evo1 }}, {{ evo2 }}, {{ evo3 }}</div>
 </template>
 
 <script>
@@ -10,16 +10,34 @@ export default {
   setup(props) {
     const evo = ref({})
     const species = ref([props.species])
+    const evoChain = ref([])
+    const evo1 = ref('')
+    const evo2 = ref('')
+    const evo3 = ref('')
+    // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id.value}.png
     console.log(species)
     const fetchEvoData = async () => {
-      console.log(props.species)
-      const response = await getResponse(`https://pokeapi.co/api/v2/evolution-chain`)
-
-      return { response }
+      const chainResponse = await getResponse(props.species.evolution_chain.url)
+      console.log(chainResponse)
+      evoChain.value = chainResponse
+      evo1.value = chainResponse.chain.species.name
+      evo2.value = chainResponse.chain.evolves_to[0].species.name
+      evo3.value = chainResponse.chain.evolves_to[0].evolves_to[0].species.name
+      const getNumberUrl = (url) => {
+        const parts = url.split('/')
+        return parts[parts.length - 2]
+      }
+      const evo1Id = getNumberUrl(chainResponse.chain.species.url)
+      console.log(chainResponse.chain.species.url)
+      console.log()
+      console.log((evo1, evo2, evo3))
     }
     onMounted(fetchEvoData)
     return {
-      evo
+      evo,
+      evo1,
+      evo2,
+      evo3
     }
   }
 }
