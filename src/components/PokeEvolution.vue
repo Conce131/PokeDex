a3ea4d67dff59453e6b8022686bf27ee7989d1e8
Conce131 <you@econcep131@gmail.com>
2023-06-13 18:08:00 +0100

    adding arrows animation

--- src/components/PokeEvolution.vue
+++ src/components/PokeEvolution.vue
@@ -3,8 +3,14 @@
     class="flex flex-row space-x-6 my-6 py-6 bg-emerald-400 bg-[url('./assets/body_bg.png')] bg-repeat w-[95%] justify-center rounded-3xl"
   >
     <div class="flex flex-wrap justify-center" v-for="level in evolutions" :key="level.level">
-      <div v-if="level.level > 0" class="shrink-0 flex align-center h-[100%]">></div>
-      <div class="p-2 max-h-[15rem]" v-for="evolution in level.evolutions" :key="evolution.id">
+      <div id="arrow" v-if="level.level > 0" class="shrink-0 flex align-center pl-8 w-[3rem] h-[100%]">
+        <span ></span>
+        <span ></span>
+        <span ></span>
+      </div>
+
+      <div><div class="p-2 max-h-[15rem]" v-for="evolution in level.evolutions" :key="evolution.id">
+
         <div class="flex shrink-1 flex-col items-center">
           <div
             class="flex shrink align-center border-emerald-800 border-8 rounded-full w-[13rem] h-[13rem] bg-emerald-50 hover:bg-gray-100 justify-center"
@@ -20,7 +26,7 @@
             {{ evolution.name }}
           </div>
         </div>
-      </div>
+      </div></div>
     </div>
   </div>
 </template>
@@ -107,6 +113,42 @@ export default {
   }
   90% {
     transform: translateY(4px);
+  }
+}
+
+#arrow {
+  cursor: pointer;
+}
+
+#arrow span {
+  width: 1vw;
+  height: 1vw;
+  border-top: 5px solid white;
+  border-left: 5px solid white;
+  transform: rotate(0deg);
+  margin: -10px;
+  animation: animate 2s infinite reverse;
+}
+
+#arrow span:nth-child(2) {
+  animation-delay: 0.2s;
+}
+
+#arrow span:nth-child(3) {
+  animation-delay: 0.4s;
+}
+
+@keyframes animate {
+  0% {
+    opacity: 0;
+    transform: rotate(135deg) translate(0px, 0px);
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+    transform: rotate(135deg) translate(20px, 20px);
   }
 }
 </style>
